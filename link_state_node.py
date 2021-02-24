d652d3337c9d69da821639e2b52197c340ddf7ea
Kathryne Tao <kathryne@Kathrynes-MacBook-Pro-20.local>
2021-02-24 00:49:04 -0600

    haygirlhay

--- link_state_node.py
+++ link_state_node.py
@@ -1,4 +1,8 @@
 from simulator.node import Node
+import json
+
+#creating a graph that keys edges to dictionary of cost and seq_num
+self.graph = {}
 
 
 class Link_State_Node(Node):
@@ -12,7 +16,28 @@ class Link_State_Node(Node):
     # Fill in this function
     def link_has_been_updated(self, neighbor, latency):
         # latency = -1 if delete a link
-        pass
+        edge = [self.id, neighbor]
+
+        if latency == -1:
+            self.graph[edge] = float('inf')
+
+        # elif edge in self.graph:
+        #     self.graph[edge]["cost"] = latency
+        #     self.graph[edge]["seq_num"] = self.get_time()
+
+        else:
+            self.graph[edge] = {"cost": latency, "seq_num": self.get_time()}
+
+
+        for e in self.graph:
+            message = {"src": e[0], 'dst': e[1], 'cost': self.graph[e]["cost"], 'seq_num': self.graph[e]["seq_num"]}
+            json_msg = json.dumps(message)
+            self.send_to_neighbor(neighbor, json_msg)
+
+        final_message = {"src": self.id, 'dst': neighbor, 'cost': self.graph[edge]['cost'], 'seq_num': self.graph[edge]['seq_num']}
+        json_final = json.dumps(final_message)
+        self.send_to_neighbors(json_final)
+
 
     # Fill in this function
     def process_incoming_routing_message(self, m):
